b5fc6c935f87915198d0d9436074203ff73f1666
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-09 15:48:38 +0530

    [sagarika]: Line length calculated

--- src/com/bridgelabz/linecomparison/LineComparsion.java
+++ src/com/bridgelabz/linecomparison/LineComparsion.java
@@ -1,5 +1,16 @@
 package com.bridgelabz.linecomparison;
 
 public class LineComparsion {
+	public static void main(String[] args) {
 
+		int L1x1 = 46;
+		int L1y1 = 64;
+		int L1x2 = 10;
+		int L1y2 = 31;
+		
+		Double LineLength1 = (Math.sqrt(Math.pow((L1x2 - L1x1),2)) + (Math.pow((L1y2 - L1y1),2)));
+		
+		System.out.println("Line Length is " +LineLength1);
+
+}
 }
