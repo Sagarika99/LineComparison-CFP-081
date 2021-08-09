435d8853758da198e3514be5c178929af85c2aa7
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-09 15:51:29 +0530

    [sagarika]; Checking of equality of line added

--- src/com/bridgelabz/linecomparison/LineComparsion.java
+++ src/com/bridgelabz/linecomparison/LineComparsion.java
@@ -7,10 +7,18 @@ public class LineComparsion {
 		int L1y1 = 64;
 		int L1x2 = 10;
 		int L1y2 = 31;
+		int L2x1 = 24;
+		int L2y1 = 24;
+		int L2x2 = 14;
+		int L2y2 = 25;
 		
 		Double LineLength1 = (Math.sqrt(Math.pow((L1x2 - L1x1),2)) + (Math.pow((L1y2 - L1y1),2)));
 		
 		System.out.println("Line Length is " +LineLength1);
+		
+		Double LineLength2 = (Math.sqrt(Math.pow((L2x2 - L2x1),2)) + (Math.pow((L2y2 - L2y1),2)));
+		
+		System.out.println(" Checking Equality of LineLength1 " + LineLength1 + " and LineLength2 " + LineLength2 + " : " +LineLength1.equals(LineLength2));
 
 }
 }
