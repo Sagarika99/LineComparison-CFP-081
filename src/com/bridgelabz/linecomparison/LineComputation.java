53dca2061b189bce9e3bf543951357d97e3653cc
Sagarika99 <68278449+Sagarika99@users.noreply.github.com>
2021-08-14 01:40:53 +0530

    Delete LineComputation.java

--- src/com/bridgelabz/linecomparison/LineComputation.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.linecomparison;
-
-public class LineComputation {
-	public static void main(String[] args) {
-		LineComparsion.LineEquals();
-		LineComparsion.LineCompareTo();
-	}
-
-}
