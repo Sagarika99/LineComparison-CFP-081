1c01a9446c316a87d847967adb0e6af5247cb44b
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-14 01:39:21 +0530

    Refractored Code using equals, compareTo and oops method

---
+++ src/com/bridgelabz/linecomparison/LineCompute.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.linecomparison;
+
+public class LineCompute {
+	public static void main(String[] args) {
+		LineComparsion.LineEquals();
+		LineComparsion.LineCompareTo();
+	}
+}
